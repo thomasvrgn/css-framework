bef4e0b7efd40fdd39d399811d9a003da8085c97
NessMC <contact@nessmc.fr>
2020-04-25 14:59:25 +0200

    🔨 Properties variable added.

---
+++ src/js/framework.js
@@ -0,0 +1,12 @@
+/*/////////////////////////////
+         CSS FRAMEWORK
+        Initialisations
+/////////////////////////////*/
+
+const properties = {
+  'clr': 'color',
+  'bg': 'background-color',
+  'sz': 'font-size',
+  'm': 'margin',
+  'p': 'padding'
+}
