c1a36cb1940fe05987b71100c9bf0b30db74380d
NessMC <contact@nessmc.fr>
2020-04-26 17:51:56 +0200

    ✨ Get all options and get option functions added.

--- src/js/framework.js
+++ src/js/framework.js
@@ -3,24 +3,27 @@
         Initialisations
 /////////////////////////////*/
 
-const properties = {
-  'clr': 'color',
-  'bg': 'background-color',
-  'sz': 'font-size',
-  'm': 'margin',
-  'p': 'padding'
+const config = {
+  properties: {},
+  breakpoints: {
+    'xl': 1440,
+    'lg': 1280,
+    'md': 960,
+    'sm': 768,
+    'xs': 480 
+  },
+  events: {
+    'hv': 'hover',
+    'fc': 'focus'
+  },
+  separator: ':',
+  project_name: 'ness',
+  colors: {
+    'black': '000000',
+    'white': 'ffffff',
+    'red'  : 'ff0055'
+  }
 }
-
-const medias = {
-  'xl': 'max-width: 1440px',
-  'lg': 'max-width: 1280px',
-  'md': 'max-width: 960px',
-  'sm': 'max-width: 768px',
-  'xm': 'max-width: 480px'
-}
-
-const project_name = 'ness'
-const separator    = ':'
 
 /*/////////////////////////////
          CSS FRAMEWORK
@@ -31,3 +34,5 @@ const elements = document.body.querySelectorAll('*')
 
 const getClassList     = () => [...elements].map(x => x = {name: x, classes: [...x.classList].filter(y => y.startsWith(project_name))}).filter(x => x.classes.length > 0)
 const getItemByClass   = classes => getClassList().filter(x => x.classes.includes(classes))
+const getAllOptions    = () => config
+const getOption        = item => getAllOptions()[Object.keys(getAllOptions()).filter(x => x === item)]
