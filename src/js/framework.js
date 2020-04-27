43a8306d12620e04110474ea2a4f421643b1abe1
NessMC <contact@nessmc.fr>
2020-04-27 14:10:18 +0200

    🔨 Breakpoint, event and property parsing added.

--- src/js/framework.js
+++ src/js/framework.js
@@ -27,6 +27,9 @@ const config = {
     'black': '000000',
     'white': 'ffffff',
     'red'  : 'ff0055'
+  },
+  properties: {
+    'bg-color': 'background-color'
   }
 }
 
@@ -66,4 +69,48 @@ const getValue    = className => getClass(className)[0].slice(getClass(className
            Formating
 /////////////////////////////*/
 
-const format = () => getClassList().map(x => x.classes.map(y => y = {project_name: y.split(':')[0] || null, property: y.split(':')[1].split('=')[0] || null, value: y.split(':')[1].split('=')[1] || null, event: y.split(':')[1].split('=')[0].match(/\((.*)\)/) !== null ? y.split(':')[1].split('=')[0].match(/\((.*)\)/)[0].split('(').join('').split(')').join('') : null}))
+const format = () => getClassList().map(x => x.classes.map(y => y = {project_name: y.split(':')[0] || null, property: y.split(':')[1].split('=')[0].split(/\((.*)\)/g).pop() || null, value: y.split(':')[1].split('=')[1] || null, event: y.split(':')[1].split('=')[0].match(/\((.*)\)/) !== null ? y.split(':')[1].split('=')[0].match(/\((.*)\)/)[0].split('(').join('').split(')').join('') : null}))
+
+/*/////////////////////////////
+         CSS FRAMEWORK
+            Parsing
+/////////////////////////////*/
+
+format().forEach(classList => {
+  classList.forEach(element => {
+    const finalClass = []
+    if (element.project_name === getOption('project_name')) {
+      if (element.event !== null) {
+        const breakpoints = getOption('breakpoints')
+        const events      = getOption('events')
+        for (const i in breakpoints) {
+          if (element.event === i) {
+            console.log('Breakpoint:', breakpoints[i] + 'px')
+          }
+        }
+        for (const i in events) {
+          if (element.event === i) {
+            console.log('Event:', events[i])
+          }
+        }
+      }
+      if (element.property !== null) {
+        const properties        = getOption('properties')
+        const properties_length = Object.keys(properties).length
+        let   counter           = 0
+        let   class_property    = ''
+        for (const i in properties) {
+          if (i !== element.property) {
+            ++counter
+            if (counter === properties_length) {
+              class_property = element.property
+            }
+          } else {
+            class_property = properties[i]
+          }
+        }
+        console.log('Property:', class_property)
+      }
+    }
+  })
+})
