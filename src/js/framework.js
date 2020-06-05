0b81de50b10b15d2c37cb26a0f8dd4c29f4bfc3e
NessMC <contact@nessmc.fr>
2020-06-05 11:18:36 +0200

    🔨 Attribute setting method added.

--- src/js/framework.js
+++ src/js/framework.js
@@ -32,127 +32,51 @@ const config = {
   }
 }
 
-/*/////////////////////////////
-         CSS FRAMEWORK
-            Reading     
-/////////////////////////////*/
+class Class {
 
-const elements         = document.body.querySelectorAll('*')
+  static element = {}
 
-const getClassList     = ()      => [...elements].map(x => x = {name: x, classes: [...x.classList].filter(y => y.startsWith(config.project_name))}).filter(x => x.classes.length > 0)
-const getItemByClass   = classes => getClassList().filter(x => x.classes.includes(classes))
-const getAllOptions    = ()      => config
-const getOption        = item    => getAllOptions()[Object.keys(getAllOptions()).filter(x => x === item)]
-
-/*/////////////////////////////
-         CSS FRAMEWORK
-             Style
-/////////////////////////////*/
-
-let style = document.createElement('style')
-document.head.appendChild(style)
-style.innerHTML += '/*/////////////////////////////\n         CSS FRAMEWORK\n              CSS\n/////////////////////////////*/\n\n'
-
-/*/////////////////////////////
-         CSS FRAMEWORK
-            Classes
-/////////////////////////////*/
-
-const createClass = (css)     => style.innerHTML += `${css}\n`
-const getClass    = className => style.innerHTML.split(/\r?\n/).slice(4).filter(x => x !== '').filter(x => x.startsWith(`.${className}`))
-const getProperty = className => getClass(className)[0].slice(getClass(className)[0].indexOf('{') + 1).split(':')[0]
-const getValue    = className => getClass(className)[0].slice(getClass(className)[0].indexOf('{') + 1).split(':')[1].split(';}').join('')
-
-/*/////////////////////////////
-         CSS FRAMEWORK
-           Formating
-/////////////////////////////*/
-
-const format = () => getClassList().map(x => x.classes.map(y => y = {project_name: y.split(':')[0] || null, property: y.split(':')[1].split('=')[0].split(/\((.*)\)/g).pop() || null, value: y.split(':')[1].split('=')[1] || null, event: y.split(':')[1].split('=')[0].match(/\((.*)\)/) !== null ? y.split(':')[1].split('=')[0].match(/\((.*)\)/)[0].split('(').join(' ').split(')').filter(x => x !== '').map(x => x.trim()) : null, full: y}))
-
-/*/////////////////////////////
-         CSS FRAMEWORK
-            Parsing
-/////////////////////////////*/
-
-format().forEach(classList  => {
-  classList.forEach(element => {
-    if (element.project_name !== config.project_name) return
-
-    let valueElements = '%media%%class%%event%{%property%:%value%;}%media end%',
-        property      = element.property,
-        value         = element.value,
-        event         = element.event,
-        full          = '.' + element.full.replace(/\(/g, '\\(').replace(/\)/g, '\\)').replace(/\=/g, '\\=').replace(/\:/g, '\\:'),
-        media         = undefined,
-        effect        = undefined
-
-    for (const param in getAllOptions()) {
-      if (property === param) {
-        for (const item in getOption(param)) {
-          if (item === value) {
-            value = getOption(param)[item]
-          }
-        }
-      }
-    }
+  static getAllElements (root = document.body) {
     
-    if (getOption('global')) {
-      for (const item in getOption('global')) {
-        if (value === item) {
-          value = getOption('global')[item]
-        }
-      }
-    }
+    return root.querySelectorAll('*')
 
-    if (getOption('properties')) {
-      for (const param in getOption('properties')) {
-        if (param === property) {
-          property = getOption('properties')[param]
-        }
-      }
-    }
+  }
 
-    if (event) {
-      if (getOption('events')) {
-        for (const param in getOption('events')) {
-          for (const item of event) {
-            if (item === param) {
-              if (!isNaN(getOption('events')[param])) media = `@media(max-width:${getOption('events')[param]}px){`
-              else effect = `:${getOption('events')[param]}`
-            }
-          }
-        }
-      }
-    }
+  static setAttribute (element = document.body.querySelectorAll('*')) {
+    [...element].map(x => x.setAttribute('fw-' + ([...Array.from(x.classList).join('')].map(x => x.charCodeAt()).reduce((acc, cur) => acc + cur)<< 19 ).toString(36), ''))
+  }
 
-    if (value) {
-      if (getOption('unity')) {
-        for (const unity in getOption('unity')) {
-          if (value.endsWith(unity)) {
-            value = value.slice(0, value.length - unity.length) + getOption('unity')[unity]
-          }
-        }
-      }
-    }
+  static createClass (content = '.error{color: red;}') {
 
-    if (media) value = value + '!important'
+  }
 
-    if (media) valueElements    = valueElements.replace('%media%', media).replace('%media end%', '}')
-    else valueElements          = valueElements.replace(/(%media%|%media end%)/g, '')
+}
 
-    if (effect) valueElements   = valueElements.replace('%event%', effect)
-    else valueElements          = valueElements.replace(/(%event%)/g, '')
+class VM extends Class{
 
-    if (full) valueElements     = valueElements.replace('%class%', full)
+  static async render (element) {
 
-    if (property) valueElements = valueElements.replace('%property%', property)
+    this.setAttribute()
 
-    if (value) valueElements    = valueElements.replace('%value%', value)
+  }
 
+}
 
-    createClass(valueElements)
-    
+class Framework extends VM {
 
-  })
-})
+  static init (options = {
+    unity: Object,
+    events: Object,
+    project_name: String,
+    color: Object,
+    properties: Object,
+    global: Object
+  }) {
+
+    this.render()
+
+  }
+
+}
+
+Framework.init(config)
